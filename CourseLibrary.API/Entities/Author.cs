826b452ab3183dc90f6a9e7b22436a40ba069537
AmmarMKamel <ammarkamel9899@gmail.com>
2026-04-23 14:51:18 +0200

    Add project files.

---
+++ CourseLibrary.API/Entities/Author.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourseLibrary.API.Entities;
+
+public class Author(string firstName, string lastName, string mainCategory)
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string FirstName { get; set; } = firstName;
+
+    [Required]
+    [MaxLength(50)]
+    public string LastName { get; set; } = lastName;
+
+    [Required]
+    public DateTimeOffset DateOfBirth { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string MainCategory { get; set; } = mainCategory;
+
+    public ICollection<Course> Courses { get; set; }
+        = [];
+}
+
---
+++ CourseLibrary.API/Entities/Course.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CourseLibrary.API.Entities;
+
+public class Course(string title)
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string Title { get; set; } = title;
+
+    [MaxLength(1500)]
+    public string? Description { get; set; }
+
+    [ForeignKey("AuthorId")]
+    public Author Author { get; set; } = null!;
+
+    public Guid AuthorId { get; set; }
+}
+
