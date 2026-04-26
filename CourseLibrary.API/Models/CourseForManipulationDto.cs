b9894a2e697796811efda7189ebec07a10904736
AmmarMKamel <ammarkamel9899@gmail.com>
2026-04-26 12:56:29 +0300

    feat: add custom attribute to validate course title and description

--- CourseLibrary.API/Models/CourseForManipulationDto.cs
+++ CourseLibrary.API/Models/CourseForManipulationDto.cs
@@ -1,8 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using CourseLibrary.API.ValidationAttributes;
+using System.ComponentModel.DataAnnotations;
 
 namespace CourseLibrary.API.Models
 {
-    public abstract class CourseForManipulationDto
+    [CourseTitleMustBeDifferentFromDescription]
+    public abstract class CourseForManipulationDto //: IValidatableObject
     {
         [Required(ErrorMessage = "You should fill out a title.")]
         [MaxLength(100, ErrorMessage = "The title shouldn't have more than 100 characters.")]
@@ -10,5 +12,15 @@ namespace CourseLibrary.API.Models
 
         [MaxLength(1500, ErrorMessage = "The description shouldn't have more than 1500 characters.")]
         public virtual string Description { get; set; } = string.Empty;
+
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Title == Description)
+        //    {
+        //        yield return new ValidationResult(
+        //            "The provided description should be different from the title.",
+        //            new[] { "Course" });
+        //    }
+        //}
     }
 }
