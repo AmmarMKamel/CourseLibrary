03ce96987c24a970e3566f24a75a3c3e5df676a2
AmmarMKamel <ammarkamel9899@gmail.com>
2026-05-02 16:46:35 +0300

    feat: replace ResponseCache with Marvin.Cache.Headers

--- CourseLibrary.API/StartupHelperExtensions.cs
+++ CourseLibrary.API/StartupHelperExtensions.cs
@@ -76,6 +76,17 @@ internal static class StartupHelperExtensions
 
         builder.Services.AddResponseCaching();
 
+        builder.Services.AddHttpCacheHeaders(
+            (expirationModelOptions) =>
+            {
+                expirationModelOptions.MaxAge = 60;
+                expirationModelOptions.CacheLocation = Marvin.Cache.Headers.CacheLocation.Private;
+            },
+            (validationModelOptions) =>
+            {
+                validationModelOptions.MustRevalidate = true;
+            });
+
         return builder.Build();
     }
 
@@ -99,6 +110,8 @@ internal static class StartupHelperExtensions
         }
 
         app.UseResponseCaching();
+
+        app.UseHttpCacheHeaders();
  
         app.UseAuthorization();
 
